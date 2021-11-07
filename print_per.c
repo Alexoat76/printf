f805a673f4eb89dafd4aa3b13d3bf975f955ebf8
joacasallas <joacasallas@gmail.com>
2021-11-07 12:36:56 -0500

    update

---
+++ print_per.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_percent - function to print percent symbol
+ * @i: char to print
+ * Return: 0 for success
+ */
+
+int print_percent(char *ptr)
+{
+	int i;
+	void ptr;
+
+	_putchar('%');
+
+	return (0);
+}
