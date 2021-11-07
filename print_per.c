5c8e7d938f1250634ddeb38767eff30768e380ad
joacasallas <joacasallas@gmail.com>
2021-11-07 12:46:50 -0500

    update

--- print_per.c
+++ print_per.c
@@ -2,13 +2,12 @@
 
 /**
  * print_percent - function to print percent symbol
- * @i: char to print
+ * @ptr: pointer of symbol received
  * Return: 0 for success
  */
 
 int print_percent(va_list *ptr)
 {
-	int i;
 	void ptr;
 
 	_putchar('%');
