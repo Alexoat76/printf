612ee23f1a4b17b890774ac3e0f271f437e82c34
joacasallas <joacasallas@gmail.com>
2021-11-07 16:52:32 -0500

    update

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - Function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ * Return: Printed characters.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ptr;
+
+	return ();
+}
--- main.h
+++ main.h
@@ -1,22 +1,38 @@
 #ifndef _MAIN_H_
-#define <stdio.h>
-#define <stdlib.h>
-#define <starg.h>
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*
+ * File: main.h
+ * Authors: Joana Casallas / Alex Arevalo
+ * email: 3361@holbertonschool  /  3915@holbertonschool.com
+ */
 
 /**
  * struct op - Struct op
- *
- * @op: The symbol
- * @f: The function associated
+ * @op: The operator.
+ * @f: The function associated.
  */
+
 typedef struct op
 {
 	char *op;
 	int (*f)(va_list);
 } op_t;
 
+/*protypes of functions*/
+
 int _putper(va_list *ptr);
 int _putchar(va_list *ptr);
 int _puts(va_list *ptr);
 
-int (*get_op_func(char *s))(va_list);
+
+/*prototype: selects the correct function to print*/
+
+int (*get_func(char *s))(va_list);
+
+
+#endif /*_MAIN_H_*/
